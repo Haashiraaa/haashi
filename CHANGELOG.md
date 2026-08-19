65084c1609a0be292115879160b7e01c592dfb59
Haashiraaa <fregeneterry3@gmail.com>
2026-08-19 14:28:30 +0100

    Add deprecation warning ahead of haashi rename

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,13 @@ All notable changes to haashi-analytics-toolkit will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+---
+
+## [v1.8.1] - 2026-08-19 - Deprecation Notice
+
+### Changed
+- Package renamed to `haashi`. This release adds a DeprecationWarning
+ on import; no functional changes.
 
 ---
 
--- src/haashi_pkg/__init__.py
+++ src/haashi_pkg/__init__.py
@@ -3,14 +3,29 @@
 # haashi_pkg/__init__.py
 
 
+
 """haashi_pkg - Data analysis, visualization, and utility toolkit."""
+
+import warnings
+
+warnings.warn(
+    "haashi_pkg is deprecated and will no longer be updated. "
+    "It has been renamed to 'haashi' (pip install haashi), though note "
+    "data_engine and plot_engine were removed in that rewrite. "
+    "See https://github.com/Haashiraaa/haashi#migration",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 
 from . import utility
 from . import data_engine
 from . import plot_engine
 from . import benchmark
 
-__version__ = "1.8.0"
+
+
+__version__ = "1.8.1"
 
 __all__ = [
     'utility',
